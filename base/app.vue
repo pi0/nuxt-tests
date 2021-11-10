bcf1ee45143cd9d0d43ec00352789d647308793b
Pooya Parsa <pyapar@gmail.com>
2021-11-11 00:27:41 +0100

    update with auto imports

--- base/app.vue
+++ base/app.vue
@@ -9,8 +9,6 @@
 </template>
 
 <script setup>
-import { defineStore } from 'pinia'
-
 const useCounterStore = defineStore('counter', {
   state: () => ({ count: Math.round(Math.random() * 100) }),
   actions: {
