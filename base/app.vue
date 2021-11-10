8c465cda0fd31b873a25954cf1cf947819b6a272
Pooya Parsa <pyapar@gmail.com>
2021-11-11 00:10:21 +0100

    add pinia

--- base/app.vue
+++ base/app.vue
@@ -1,11 +1,29 @@
 <template>
   <div>
-    Hello Nuxt {{ $config.nuxtVersion}}!
+    Hello Nuxt {{ $config.nuxtVersion }}!<br>
+
+    counter: {{ counter.count }} <button @click="counter.increment">
+      +
+    </button>
   </div>
 </template>
 
 <script>
-export default {
+import { defineStore } from 'pinia'
 
+const useCounterStore = defineStore('counter', {
+  state: () => ({ count: Math.round(Math.random() * 100) }),
+  actions: {
+    increment () {
+      this.count++
+    }
+  }
+})
+
+export default {
+  setup () {
+    const counter = useCounterStore()
+    return { counter }
+  }
 }
 </script>
--- base/nuxt.config.ts
+++ base/nuxt.config.ts
@@ -1,3 +1,5 @@
 export default {
-  modules: []
+  buildModules: [
+    '@pinia/nuxt'
+  ]
 }
